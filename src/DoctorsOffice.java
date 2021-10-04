ea1eb6f762df0d4af110d4ccbb66b2b540b4d3f3
“Mamofm” <jmamo603@gmail.com>
2021-10-04 16:49:33 -0400

    first commit

---
+++ src/DoctorsOffice.java
@@ -0,0 +1,15 @@
+public class DoctorsOffice {
+    public static void main(String[] args) {
+
+        String patientName="Stacey Manson";
+        boolean hasInsurance=false;
+        double temp=102.02;
+        int numVisits=5;
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: "+patientName);
+        System.out.println("Patient has insurance: " + hasInsurance);
+        System.out.println("Body temperature: " + temp);
+        System.out.println("Number of visits: " + numVisits);
+    }
+}
